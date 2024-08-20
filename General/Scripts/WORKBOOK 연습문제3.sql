754eb78eda856ec3e90a293b8633e05c51184a28
sungyeopkim11 <777sy777@naver.com>
2024-08-20 17:44:41 +0900

    20240820 수업 내용 및 WORKBOOK 자습 커밋

---
+++ General/Scripts/WORKBOOK 연습문제3.sql
@@ -0,0 +1,95 @@
+-- 1번
+-- 학생이름과 주소지를 조회하시오
+-- 단, 출력 헤더는 "학생 이름", "주소지"로 하고, 정렬은 이름으로 오름차순 정렬
+SELECT STUDENT_NAME "학생이름", STUDENT_ADDRESS "주소지"
+FROM TB_STUDENT
+ORDER BY STUDENT_NAME;
+
+
+-- 2번
+-- 휴학중인 학생들의 이름과 주민번호를 나이가 적은 순서 조회하시오.
+SELECT STUDENT_NAME, STUDENT_SSN
+FROM TB_STUDENT
+WHERE ABSENCE_YN = 'Y'
+ORDER BY STUDENT_SSN DESC;
+
+
+-- 3번
+-- 주소지가 강원도나 경기도인 학생들 중 1900년대 학번을 가진 학생들의
+-- 이름과 학번, 주소를 이름 오름차순으로 조회하시오.
+-- 단, 출력헤더에는 "학생이름", "학번", "거주지 주소"가 출력되도록 한다.
+SELECT 
+	STUDENT_NAME 학생이름, 
+	STUDENT_NO 학번, 
+	STUDENT_ADDRESS "거주지 주소"
+FROM TB_STUDENT
+WHERE STUDENT_ADDRESS LIKE '경기%'
+AND   STUDENT_NO NOT LIKE 'A%'
+ORDER BY STUDENT_NAME;
+
+
+-- 4번
+-- 현재 법학과 교수의 이름, 주민등록 번호를 나이가 많은 순서부터 조회하시오.
+SELECT PROFESSOR_NAME, PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE DEPARTMENT_NO = '005'
+ORDER BY PROFESSOR_SSN;
+
+
+-- 5번
+-- 2004년 2학기에 'C3118100' 과목을 수강한 학생들의 학점을 조회하려고 한다. 
+-- 학점이 높은 학생부터 표시하고, 학점이 같으면 학번이 낮은 학생부터 조회하시오.
+-- (참고) 소수점 아래 2자리까지 0으로 표현 : TO_CHAR(NUMBER, 'FM9.00')
+-- (FM : 조회 결과 양쪽 공백 제거)
+SELECT STUDENT_NO, TO_CHAR(POINT, 'FM9.00')
+FROM TB_GRADE
+WHERE TERM_NO = '200402'
+AND   CLASS_NO = 'C3118100'
+ORDER BY POINT DESC, STUDENT_NO;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
