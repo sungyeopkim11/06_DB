701f394473352781325baeab36f4d011625b49ec
sungyeopkim11 <777sy777@naver.com>
2024-10-16 16:02:46 +0900

    커밋

--- General/Scripts/Script-10.sql
+++ General/Scripts/Script-10.sql
@@ -215,3 +215,484 @@ CHECK(BOARD_DEL_FL IN ('Y', 'N'));
 ALTER TABLE "COMMENT"
 ADD CONSTRAINT "CHK_COMMENT_DEL_FL"
 CHECK(COMMENT_DEL_FL IN ('Y','N'));
+
+-- -------------------------------------------------
+
+INSERT INTO BOARD_TYPE VALUES(1, '공지 사항');
+--INSERT INTO BOARD_TYPE VALUES(2, '자유 게시판');
+INSERT INTO "BOARD_TYPE" VALUES(2, '자유 게시판');
+INSERT INTO BOARD_TYPE VALUES(3, '정보 게시판');
+COMMIT;
+
+SELECT * FROM BOARD_TYPE;
+
+--------------------------------------------------
+
+/* 게시글 번호 시퀀스 생성 */
+CREATE SEQUENCE SEQ_BOARD_NO NOCACHE;
+
+/* PL/SQL을 이용해서 BOARD 테이블에 샘플 데이터 삽입 */
+BEGIN
+		FOR I IN 1..2000 LOOP
+			INSERT INTO BOARD
+			VALUES(
+					SEQ_BOARD_NO.NEXTVAL,
+					SEQ_BOARD_NO.CURRVAL || '번째 게시글',
+					SEQ_BOARD_NO.CURRVAL || '번째 게시글 내용 입니다',
+					DEFAULT, DEFAULT, DEFAULT, DEFAULT,
+					1, -- 회원 번호
+					CEIL( DBMS_RANDOM.VALUE(0,3) )
+			);
+			
+		END LOOP;
+		
+END;
+
+SELECT COUNT(*) FROM BOARD;
+
+COMMIT;
+
+-- 게시판 종류 별로 샘플 데이터 개수 확인
+SELECT BOARD_CODE, COUNT(*)
+FROM BOARD
+GROUP BY BOARD_CODE
+ORDER BY BOARD_CODE ASC;
+
+-------------------------------------------------
+/* 댓글 번호 시퀀스 생성 */
+CREATE SEQUENCE SEQ_COMMENT_NO NOCACHE;
+
+/*  */
+BEGIN
+		FOR I IN 1..3000 LOOP
+			INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					SEQ_COMMENT_NO.CURRVAL || '번째 댓글',
+					DEFAULT, DEFAULT,
+					1,
+					CEIL(DBMS_RANDOM.VALUE(0, 1999) ),
+					NULL
+			);
+		END LOOP;
+		
+END;
+
+SELECT COUNT(*) FROM "COMMENT";
+COMMIT;
+
+
+-- 게시글 번호별 작성된 댓글 개수 조회
+SELECT BOARD_NO, COUNT(*)
+FROM "COMMENT"
+GROUP BY BOARD_NO
+ORDER BY BOARD_NO DESC;
+
+
+
+
+--------------------------------------------
+/* 특정 게시판에
+ * 삭제되지 않은 게시글 목록 조회
+ * 
+ * - 조회된 행번호 : ROWNUM 또는 ROW_NUMBER() 이용
+ * 	
+ *  게시글 번호, 제목, 조회수, 작성일 : BOARD 테이블 컬럼
+ * 
+ *   작성자 닉네임 : MEMBER 테이블 
+ * 
+ *   댓글 수 : COMMENT 테이블에서 BOARD_NO 별 댓글 개수 COUNT(*)
+ *   좋아요 개수 : BOARD_LIKE 테이블에서 BOARD_NO 별 댓글 개수 COUNT(*)
+ * 
+ * - 작성일은 몇 초/분/시간 전 또는 YYYY-MM--DD 형식으로 조회
+ * 
+ * - 가장 최근 글이 제일 처음 조회
+ * 	 (시퀀스 번호로 만들어진 BOARD_NO를 이용)
+ *  */
+
+-- ROW_NUMBER() OVER (ORDER BY BOARD_NO ASC) AS "RNUM",
+--> BOARD_NO 오름차순으로 정렬 후 조회되ㅏㄴ 행의 번호를 지정
+--   + 해당 컬럼 별칭을 "RNUM" 지정
+-->  연속 되지 않은 PK 대신 연속된 숫자를 만들려고 사용!
+
+-- 사관 서브 쿼리 해석 순서
+-- 1) 메인 쿼리 1행 해석
+-- 2) 서브 쿼리에서 메인 쿼리 1행 조회 결과를 이용 -> 해석
+-- 2) 다시 메인 쿼리 해석
+
+-- DATE 타입 끼리 - 연산 시 결과 == 몇 일 차이(일 단위)
+
+SELECT 
+		ROW_NUMBER() OVER (ORDER BY BOARD_NO ASC) AS "RNUM",
+		BOARD_NO,
+		BOARD_TITLE,
+		READ_COUNT,
+		MEMBER_NICKNAME,
+		(SELECT COUNT(*) FROM "COMMENT" C
+		 WHERE C.BOARD_NO = B.BOARD_NO ) AS "COMMENT_COUNT",
+		
+		(SELECT COUNT(*) FROM BOARD_LIKE L
+		 WHERE L.BOARD_NO = B.BOARD_NO) AS "LIKE_COUNT",
+		 
+		 CASE
+		 	WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1 / 24 / 60
+		 	THEN FLOOR( (CURRENT_DATE - BOARD_WRITE_DATE)
+		 	             * 24 * 60 * 60 ) || '초 전'
+		 	             
+		 	WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1 / 24
+		 	THEN FLOOR( (CURRENT_DATE - BOARD_WRITE_DATE)
+		 	             * 24 * 60 ) || '분 전'        
+		 	             
+     	WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1 
+		 	THEN FLOOR( (CURRENT_DATE - BOARD_WRITE_DATE)
+		 	             * 24  ) || '시간 전'
+		 	             
+		 	ELSE TO_CHAR(BOARD_WRITE_DATE, 'YYYY-MM-DD')
+		 
+		 END AS "BOARD_WRITE_DATE"
+		 
+FROM		"BOARD" B
+JOIN "MEMBER" M ON (B.MEMBER_NO = M.MEMBER_NO)
+WHERE BOARD_DEL_FL = 'N' -- 삭제 안된 글
+AND   BOARD_CODE = 1    -- 게시판 종류
+ORDER BY RNUM DESC;
+
+
+INSERT INTO BOARD
+			VALUES(
+					SEQ_BOARD_NO.NEXTVAL,
+					SEQ_BOARD_NO.CURRVAL || '번째 게시글',
+					SEQ_BOARD_NO.CURRVAL || '번째 게시글 내용 입니다',
+					DEFAULT, DEFAULT, DEFAULT, DEFAULT,
+					1, -- 회원 번호
+					1
+);
+
+
+SELECT COUNT(*)
+   		FROM "BOARD"
+   		WHERE BOARD_CODE = 3
+   		AND   BOARD_DEL_FL = 'N';
+
+
+SELECT SEQ_BOARD_NO.NEXTVAL FROM DUAL;
+
+-- 이미지 번호 생성용 시퀀스
+CREATE SEQUENCE SEQ_IMG_NO NOCACHE;
+
+/* 여러 행을 한 번에 INSERT 하는 방법
+ * 
+ * 1. INSERT ALL 구문
+ * 2. INSERT + SUBQUERY
+ * 
+ * [문제점] 위에 두 방법 다 SEQUENCE를 직접 사용 불가
+ *  */
+
+-- SEQ_IMG_NO 시퀀스의 다음 값을 반환하는 함수 생성
+CREATE OR REPLACE FUNCTION NEXT_IMG_NO
+
+-- 반환형
+RETURN NUMBER
+
+-- 사용할 변수
+IS IMG_NO NUMBER;
+
+BEGIN 
+	SELECT SEQ_IMG_NO.NEXTVAL 
+	INTO IMG_NO
+	FROM DUAL;
+
+	RETURN IMG_NO;
+END;
+;
+
+INSERT INTO "BOARD_IMG"
+(
+SELECT 
+		NEXT_IMG_NO(),
+		'/images/board/',
+		'원본명',
+		'변경명',
+		1,
+		2000
+FROM DUAL
+
+UNION ALL
+
+SELECT 
+		NEXT_IMG_NO(),
+		'/images/board/',
+		'원본명',
+		'변경명',
+		1,
+		2000
+FROM DUAL
+);
+
+ROLLBACK;
+
+SELECT * FROM "BOARD";
+
+SELECT 
+		ROW_NUMBER() OVER (ORDER BY BOARD_NO ASC) AS "RNUM",
+		BOARD_NO,
+		BOARD_TITLE,
+		READ_COUNT,
+		MEMBER_NICKNAME,
+		(SELECT COUNT(*) FROM "COMMENT" C
+		 WHERE C.BOARD_NO = B.BOARD_NO ) AS "COMMENT_COUNT",
+		
+		(SELECT COUNT(*) FROM BOARD_LIKE L
+		 WHERE L.BOARD_NO = B.BOARD_NO) AS "LIKE_COUNT",
+		 
+		 
+		 CASE
+		 	WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1 / 24 / 60
+		 	THEN FLOOR( (CURRENT_DATE - BOARD_WRITE_DATE)
+		 	             * 24 * 60 * 60 ) || '초 전'
+		 	             
+		 	WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1 / 24
+		 	THEN FLOOR( (CURRENT_DATE - BOARD_WRITE_DATE)
+		 	             * 24 * 60 ) || '분 전'        
+		 	             
+     	WHEN CURRENT_DATE - BOARD_WRITE_DATE < 1 
+		 	THEN FLOOR( (CURRENT_DATE - BOARD_WRITE_DATE)
+		 	             * 24  ) || '시간 전'
+		 	             
+		 	ELSE TO_CHAR(BOARD_WRITE_DATE, 'YYYY-MM-DD')
+		 
+		 END AS "BOARD_WRITE_DATE"
+		 , 
+		 (SELECT IMG_PATH || IMG_RENAME
+		  FROM BOARD_IMG I
+		  WHERE IMG_ORDER = 0
+		  AND I.BOARD_NO = B.BOARD_NO) AS THUMBNAIL
+		 
+	FROM		"BOARD" B
+	JOIN "MEMBER" M ON (B.MEMBER_NO = M.MEMBER_NO)
+	WHERE BOARD_DEL_FL = 'N' 
+	AND   BOARD_CODE = 1  
+	ORDER BY RNUM DESC;
+
+------------------------------------------------------
+/* 게시글 상세 조회 */
+SELECT 
+		BOARD_NO, BOARD_TITLE, BOARD_CONTENT,
+		BOARD_CODE, READ_COUNT, B.MEMBER_NO,
+		MEMBER_NICKNAME, PROFILE_IMG,
+		
+		TO_CHAR(BOARD_WRITE_DATE, 'YYYY"년" MM"월" DD"일" HH24:MI:SS')
+				AS BOARD_WRITE_DATE,
+
+		TO_CHAR(BOARD_UPDATE_DATE, 'YYYY"년" MM"월" DD"일" HH24:MI:SS')
+				AS BOARD_UPDATE_DATE,
+				
+		(SELECT COUNT(*)
+		 FROM BOARD_LIKE L
+		 WHERE L.BOARD_NO = 2017) AS LIKE_COUNT,
+		 
+		(SELECT IMG_PATH || IMG_RENAME
+		 FROM BOARD_IMG I
+		 WHERE I.BOARD_NO = 2017 ) AS THUMBNAIL
+		
+FROM "BOARD" B
+JOIN "MEMBER" M ON (B.MEMBER_NO = M.MEMBER_NO)
+WHERE BOARD_NO = 2017
+AND   BOARD_CODE = 1;
+
+------------------------------------------------------
+/* 댓글 샘플 데이터 추가 */
+INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'부모 댓글 1',
+					DEFAULT, DEFAULT,
+					2,
+					2020,
+					NULL
+			);
+
+		INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'부모 댓글 2',
+					DEFAULT, DEFAULT,
+					2,
+					2020,
+					NULL
+			);
+
+		INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'부모 댓글 3',
+					DEFAULT, DEFAULT,
+					2,
+					2020,
+					NULL
+			);
+
+/* 특정 게시글의 댓글 목록 조회 + 계층형 쿼리 */
+SELECT COMMENT_NO, PARENT_COMMENT_NO, COMMENT_CONTENT
+FROM "COMMENT"
+WHERE BOARD_NO = 2020;
+
+COMMIT;
+
+-- 부모 댓글1의 자식 댓글
+INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'자식 댓글 1-1',
+					DEFAULT, DEFAULT,
+					2,2020,3001);
+
+		INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'자식 댓글 1-2',
+					DEFAULT, DEFAULT,
+					2,
+					2020,
+					3001
+			);
+		
+		INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'자식 댓글 2-1',
+					DEFAULT, DEFAULT,
+					2,
+					2020,
+					3003
+			);
+
+INSERT INTO "COMMENT"
+	VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'자식 댓글 2-2',
+					DEFAULT, DEFAULT,
+					2,
+					2020,
+					3003
+			);
+INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'후손 댓글 1-1-1',
+					DEFAULT, DEFAULT,
+					2,2020,3005);
+INSERT INTO "COMMENT"
+			VALUES(
+					SEQ_COMMENT_NO.NEXTVAL,
+					'후손 댓글 1-1-2',
+					DEFAULT, DEFAULT,
+					2,2020,3005);
+
+COMMIT;
+
+/* 특정 게시글의 댓글 목록 조회 + 계층형 쿼리 */
+-- LEVEL 컬럼 : 현재 계층 레벨 출력
+SELECT LEVEL, COMMENT_NO, PARENT_COMMENT_NO, COMMENT_CONTENT
+FROM "COMMENT"
+WHERE BOARD_NO = 2020
+
+/* 계층형 쿼리 코드 추가 */
+-- PARENT_COMMENT_NO 값이 NULL인 행이 계층의 시작이다
+-- LEVEL == 1
+START WITH PARENT_COMMENT_NO IS NULL
+
+-- PARENT_COMMENT_NO 값이 COMMENT_NO 와 같은 행을
+-- 자식으로 지정하여 연결
+CONNECT BY PRIOR COMMENT_NO = PARENT_COMMENT_NO
+
+-- 형제들(같은 레벨) 간의 정렬 순서를 COMMENT_NO 오름차순으로 지정
+ORDER SIBLINGS BY COMMENT_NO ASC;
+
+/* 실제 사용할 댓글 목록 조회 + 계층형 쿼리 */
+SELECT LEVEL, C.* FROM
+	(SELECT COMMENT_NO, COMMENT_CONTENT,
+	    TO_CHAR(COMMENT_WRITE_DATE, 'YYYY"년" MM"월" DD"일" HH24"시" MI"분" SS"초"') COMMENT_WRITE_DATE,
+	    BOARD_NO, MEMBER_NO, MEMBER_NICKNAME, PROFILE_IMG, PARENT_COMMENT_NO, COMMENT_DEL_FL
+	FROM "COMMENT"
+	JOIN MEMBER USING(MEMBER_NO)
+	WHERE BOARD_NO = 2020) C
+WHERE COMMENT_DEL_FL = 'N'
+OR 0 != (SELECT COUNT(*) FROM "COMMENT" SUB
+				WHERE SUB.PARENT_COMMENT_NO = C.COMMENT_NO
+				AND COMMENT_DEL_FL = 'N')
+START WITH PARENT_COMMENT_NO IS NULL
+CONNECT BY PRIOR COMMENT_NO = PARENT_COMMENT_NO
+ORDER SIBLINGS BY COMMENT_NO
+;
+
+
+INSERT INTO "BOARD_TYPE"
+VALUES(4, '테스트');
+
+COMMIT;
+
+
+SELECT COUNT(*)
+FROM "BOARD"
+JOIN "MEMBER" USING ("MEMBER_NO") -- 작성자 검색
+WHERE BOARD_CODE = 1
+AND   BOARD_DEL_FL = 'N';
+
+-- 제목 검색인 경우
+-- AND   BOARD_TITLE LIKE '%' || '11' || '%'
+
+-- 내용 검색인 경우
+-- AND   BOARD_CONTENT LIKE '%' || '11' || '%'
+
+-- 제목 또는 내용 검색인 경우
+--AND  (BOARD_TITLE  LIKE '%' || '11' || '%'
+  -- OR    BOARD_CONTENT   LIKE '%' || '11' || '%')
+
+-- 작성자 검색
+-- AND MEMBER_NICKNAME LIKE '%' || '11' || '%'
+
+
+-------------------------------------------------
+
+/* 현재 게시글이 속해있는 페이지 번호 조회 */
+SELECT RNUM, BOARD_NO, CEIL(RNUM/10) CP
+FROM
+	(SELECT 
+		ROW_NUMBER() OVER(ORDER BY BOARD_NO DESC) RNUM
+		,BOARD_NO
+	FROM "BOARD"
+	WHERE BOARD_CODE = 1
+	AND   BOARD_DEL_FL = 'N')
+WHERE BOARD_NO = 500;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
